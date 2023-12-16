730349e0268ee73be9585e61bb489b6b4d006be2
Maarten Hogeveen <mhogeveen94@gmail.com>
2023-12-16 14:43:58 +0100

    feat: change env var

--- sites/cms/src/payload.config.ts
+++ sites/cms/src/payload.config.ts
@@ -4,15 +4,11 @@ import { mongooseAdapter } from "@payloadcms/db-mongodb";
 import { webpackBundler } from "@payloadcms/bundler-webpack";
 import { slateEditor } from "@payloadcms/richtext-slate";
 import { buildConfig } from "payload/config";
-import dotenv from "dotenv";
 
 import Users from "./collections/Users";
 import Pages from "./collections/Pages";
 
-dotenv.config();
-
-const PUBLIC_DOMAIN = process.env.PUBLIC_DOMAIN;
-const isProd = () => process.env.NODE_ENV === "production";
+const PUBLIC_DOMAIN = process.env.PAYLOAD_PUBLIC_SERVER_URL;
 
 export default buildConfig({
   admin: {
@@ -21,7 +17,7 @@ export default buildConfig({
   },
   cors: ["http://localhost:3000", PUBLIC_DOMAIN],
   csrf: ["http://localhost:3000", PUBLIC_DOMAIN],
-  serverURL: isProd() ? PUBLIC_DOMAIN : "http://localhost:4000",
+  serverURL: PUBLIC_DOMAIN || "http://localhost:4000",
   editor: slateEditor({}),
   collections: [Users, Pages],
   typescript: {
