12e78d623d0019957decf504c5a6a1186e5ddd8a
Maarten Hogeveen <mhogeveen94@gmail.com>
2023-12-16 14:23:48 +0100

    feat: add public_domain env var to config

--- sites/cms/src/payload.config.ts
+++ sites/cms/src/payload.config.ts
@@ -4,18 +4,24 @@ import { mongooseAdapter } from "@payloadcms/db-mongodb";
 import { webpackBundler } from "@payloadcms/bundler-webpack";
 import { slateEditor } from "@payloadcms/richtext-slate";
 import { buildConfig } from "payload/config";
+import dotenv from "dotenv";
 
 import Users from "./collections/Users";
 import Pages from "./collections/Pages";
+
+dotenv.config();
+
+const PUBLIC_DOMAIN = process.env.PUBLIC_DOMAIN;
+const isProd = () => process.env.NODE_ENV === "production";
 
 export default buildConfig({
   admin: {
     user: Users.slug,
     bundler: webpackBundler(),
   },
-  cors: ["http://localhost:3000"],
-  csrf: ["http://localhost:3000"],
-  serverURL: "http://localhost:4000",
+  cors: ["http://localhost:3000", PUBLIC_DOMAIN],
+  csrf: ["http://localhost:3000", PUBLIC_DOMAIN],
+  serverURL: isProd() ? PUBLIC_DOMAIN : "http://localhost:4000",
   editor: slateEditor({}),
   collections: [Users, Pages],
   typescript: {
